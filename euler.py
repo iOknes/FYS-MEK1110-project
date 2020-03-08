6298ca82796f3977eca8eb827e58e828b0505e7c
Iver Oknes <iver@oknes.no>
2020-03-09 00:26:34 +0100

    Add energy calulations to euler.py and eulerc.py Warning: current potential energy is twice of what is expected. This will be added as an issue.

--- euler.py
+++ euler.py
@@ -28,3 +28,24 @@ for i in r:
     track.write(f"{len(i)}\n\n")
     for j in i:
         track.write(f"Ar {j[0]} {j[1]} {j[2]}\n")
+
+#Calculate energies for all particles
+#Kinetic energy
+ek = np.sum(np.linalg.norm(v, axis=2)**2 / 2, axis=1)
+#Potential energy
+d = np.empty((N, pN, pN-1, 3), dtype="float64")
+print(np.shape(d))
+for i in range(N):
+    for j in range(pN):
+        r_ = np.array(r[i], copy=True)
+        r_[[0,j]] = r_[[j,0]]
+        d[i,j] = r_[0] - r_[1:]
+ep = np.sum(np.sum(p(np.linalg.norm(d, axis=3)), axis=2), axis=1)
+
+#Generate time array and plot energies
+t = np.linspace(0, T, N)
+plt.plot(t, ek, label="Kinetic energy")
+plt.plot(t, ep, label="Potential energy")
+plt.plot(t, ep + ek, label="Total energy")
+plt.legend()
+plt.show()
