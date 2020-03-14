e75e80cc5202b482cd9a80a37cf437c3f0d005b5
Iver Oknes <iver@oknes.no>
2020-03-14 23:18:07 +0100

    Update potential to be zero for distance greater than 3 sigma. Fix kinetic energy bug by accounting for number of particles (Resolve issue #1).

--- potential.py
+++ potential.py
@@ -8,10 +8,26 @@ class LJP:
         self.sigma = sgm
     
     def __call__(self, r):
-        return 4 * self.epsilon * ((self.sigma / r)**12 - (self.sigma / r)**6)
-    
+        if type(r) is float or type(r) is int:
+            if r < 3:
+                return 4 * self.epsilon * ((self.sigma / r)**12 - (self.sigma / r)**6)
+            else:
+                return 0
+        elif type(r) is np.ndarray:
+            print(r)
+            r_ = r > 3
+            print(r_)
+            r = 4 * self.epsilon * ((self.sigma / r)**12 - (self.sigma / r)**6)
+            print(r)
+            r[r_] = 0
+            print(r)
+            return r
+
     #Returns the acceleration excerten on r1 by r2
     def acc(self, r1, r2, m1):
         r = r1 - r2
         rn = norm(r)
-        return 24 * self.epsilon / m1 * (2 * (self.sigma / rn)**12 - (self.sigma / rn)**6) * r / rn**2
+        if rn < 3:
+            return 24 * self.epsilon / m1 * (2 * (self.sigma / rn)**12 - (self.sigma / rn)**6) * r / rn**2
+        else:
+            return 0
--- velver.py
+++ velver.py
@@ -3,18 +3,19 @@ import matplotlib.pyplot as plt
 from potential import LJP
 
 p = LJP(1,1)
+m = 1
 
 dt = 1e-2
 T = 10
 N = int(T/dt)
-pN = 4
+pN = 2
 
 r = np.empty([N,pN,3], dtype="float64")
 v = np.zeros_like(r)
 
-#r[0] = [(0,0,0),(0.95,0,0)]
+r[0] = [(0,0,0),(1.5,0,0)]
 #r[0] = [(0,0,0), (1.5,0,0), (-1.5,0,0)]
-r[0] = [(0,0,0), (1.5,0,0), (-1.25,-1.25,0), (0,0,1.5)]
+#r[0] = [(0,0,0), (1.5,0,0), (-1.25,-1.25,0), (0,0,1.5)]
 
 track = open("track_r.xyz", "w")
 
@@ -24,14 +25,14 @@ for i in range(N-1):
     a_ = np.zeros((pN, 3))
     for j in range(pN):
         for k in range(j+1, pN):
-            a__ = p.acc(r[i,j], r[i,k], 1)
+            a__ = p.acc(r[i,j], r[i,k], m)
             a[j] += a__
             a[k] -= a__
     for j in range(pN):
         r[i+1,j] = r[i,j] + (v[i,j] * dt) + ((a[j] / 2) * dt**2)
     for j in range(pN):
         for k in range(j+1, pN):
-            a__ = p.acc(r[i+1,j], r[i+1,k], 1)
+            a__ = p.acc(r[i+1,j], r[i+1,k], m)
             a[j] += a__
             a[k] -= a__
     for j in range(pN):
@@ -44,7 +45,7 @@ for i in r:
 
 #Calculate energies for all particles
 #Kinetic energy
-ek = np.sum(np.linalg.norm(v, axis=2)**2 / 2, axis=1)
+ek = np.sum(pN * m * np.linalg.norm(v, axis=2)**2 / 2, axis=1)
 #Potential energy
 d = np.empty((N, pN, pN-1, 3), dtype="float64")
 print(np.shape(d))
@@ -55,12 +56,10 @@ for i in range(N):
         d[i,j] = r_[0] - r_[1:]
 ep = np.sum(np.sum(p(np.linalg.norm(d, axis=3)), axis=2), axis=1)
 
-"""
 #Generate time array and plot energies
 t = np.linspace(0, T, N)
 plt.plot(t, ek, label="Kinetic energy")
-plt.plot(t, ep/2, label="Potential energy")
-plt.plot(t, ep/2 + ek, label="Total energy")
+plt.plot(t, ep, label="Potential energy")
+plt.plot(t, ep + ek, label="Total energy")
 plt.legend()
 plt.show()
-"""
