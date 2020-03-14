ab6434128917e7766155a6360f05d757e5541adb
Iver Oknes <iver@oknes.no>
2020-03-14 23:04:54 +0100

    Revert changes in potential back to previous version.

--- potential.py
+++ potential.py
@@ -8,16 +8,10 @@ class LJP:
         self.sigma = sgm
     
     def __call__(self, r):
-        if r < 3:
-            return 4 * self.epsilon * ((self.sigma / r)**12 - (self.sigma / r)**6)
-        else:
-            return 0
-            
+        return 4 * self.epsilon * ((self.sigma / r)**12 - (self.sigma / r)**6)
+
     #Returns the acceleration excerten on r1 by r2
     def acc(self, r1, r2, m1):
         r = r1 - r2
         rn = norm(r)
-        if rn < 3:
-            return 24 * self.epsilon / m1 * (2 * (self.sigma / rn)**12 - (self.sigma / rn)**6) * r / rn**2
-        else:
-            return 0
+        return 24 * self.epsilon / m1 * (2 * (self.sigma / rn)**12 - (self.sigma / rn)**6) * r / rn**2
