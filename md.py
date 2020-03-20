0d77a7c7eff459dad316d5bbaaeca2353faa9d73
Iver Oknes <iver@oknes.no>
2020-03-20 19:14:07 +0100

    Fix MD class by fixing particle adding methods. Add track method for internal and external use.

--- md.py
+++ md.py
@@ -6,20 +6,33 @@ class MD:
         self.epsilon = epsilon
         self.sigma = sigma
         self.u = LJP(epsilon, sigma)
-        self.r = np.empty(0)
-        self.v = np.empty(0)
+        self.r_init = np.empty(0, dtype="float64")
+        self.v_init = np.empty(0, dtype="float64")
         self.pN = 0
+        self.trackfile = open("track_r.xyz", "w")
 
     #Takes one position vector and one velocity vector of any equal dimension
     def add_molecule(self, position, velocity):
-        np.concatenate(self.r, np.array(position))
-        np.concatenate(self.v, np.array(velocity))
         self.pN += 1
+        self.r_init = np.split(np.concatenate((self.r_init, np.array(position)), axis=None), self.pN)
+        self.v_init = np.split(np.concatenate((self.v_init, np.array(velocity)), axis=None), self.pN)
 
     def add_molecules(self, positions, velocities):
         for i in range(len(positions)):
             self.add_molecule(positions[i], velocities[i])
     
+    def track(self, r):
+        self.trackfile.write(f"{len(r)}\ntype x y z\n")
+        for i in r:
+            self.trackfile.write(f"Ar {i[0]} {i[1]} {i[2]}\n")
+
+    """
+    Runs the simulation for a given time step, time and mass using the Velocity Verlet method
+    Arguments:
+    dt: Time step
+    T: End time
+    m: Mass of particles
+    """
     def solve(self, dt, T, m):
         p = self.u
         pN = self.pN
@@ -28,10 +41,10 @@ class MD:
         r = np.empty([N,pN,3], dtype="float64")
         v = np.zeros_like(r)
 
-        r[0] = self.r
-        v[0] = self.v
+        r[0] = self.r_init
+        v[0] = self.v_init
 
-        track = open("track_r.txt", "w")
+        self.track(r[0])
 
         for i in range(N-1):
             a = np.zeros((pN, 3))
@@ -51,7 +64,16 @@ class MD:
             for j in range(pN):
                 v[i+1,j] = v[i,j] + (a[j] + a_[j]) / 2 * dt
 
-        for i in r:
-            track.write(f"{len(i)}\ntype x y z\n")
-            for j in i:
-                track.write(f"Ar {j[0]} {j[1]} {j[2]}\n")
+            self.track(r[i+1])
+
+        return r, v
+
+def md_example():
+    md = MD(1, 1)
+    r = [[0,0,0],[1.5,0,0]]
+    v = np.zeros((2,3))
+    md.add_molecules(r, v)
+    r, v = md.solve(1e-2, 10, 1)
+
+if __name__ == "__main__":
+    md_example()
